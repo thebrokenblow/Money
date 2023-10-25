cdc3425039fe7d384af44b6bc0d10b64718d4771
Artem Krasov <artkrasov@gmail.com>
2023-10-25 18:56:28 +0300

    added tests and removed side effects

--- src/main/java/org/example/Dollar.java
+++ src/main/java/org/example/Dollar.java
@@ -6,7 +6,15 @@ public class Dollar {
         this.amount = amount;
     }
 
-    public void times(int multiplier) {
-        amount *= multiplier;
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Dollar dollar) {
+            return amount == dollar.amount;
+        }
+        return false;
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -7,10 +7,8 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    public void testApp()
-    {
-        Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(five.amount, 10);
+    public void testApp() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
