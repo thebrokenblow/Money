7c314550203a663730a9db103f281a61d3e0b643
Artem Krasov <artkrasov@gmail.com>
2023-10-25 22:58:14 +0300

    Added class Franc

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -7,8 +7,14 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    public void testApp() {
-        assertEquals(new Dollar(5), new Dollar(5));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    public void testDollarApp() {
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+    public void testFrancApp() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 }
